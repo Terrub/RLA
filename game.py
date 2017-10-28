1e7016f50be99f20253d9f3e1fb5df5f04a6c3b0
Robert Page <page.synid@gmail.com>
2017-10-28 13:56:01 +0200

    Adds game module

---
+++ game.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+"""
+The actual game module.
+
+This should contain values or references to all the game related states and
+info that it needs to make the game work and playable.
+"""
+
+
+class Game:
+    pass
