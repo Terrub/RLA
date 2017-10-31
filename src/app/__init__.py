0e3473911f595059dcb3a86a84d0e7aa2df7893e
Robert Page <page.synid@gmail.com>
2017-10-31 21:20:07 +0100

    Removes overeager addition of display object

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,7 +2,6 @@
 """
 Initialisation of the RLA game
 """
-from src.app.display import Display
 from src.app.game import Game
 
 
@@ -10,8 +9,6 @@ def initialise():
     """
     Starts the game
     """
-
-    display = Display()
     game = Game()
 
 if __name__ == '__main__':
